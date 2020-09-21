fc455a8e0267827f336e7b4172954533fd9caffa
yongsokheng <yongsokheng@gmail.com>
2020-09-21 23:53:21 +0700

    Add gem rank model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
+  include RankedModel
+  ranks :row_order
+
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_135011) do
+ActiveRecord::Schema.define(version: 2020_09_21_151144) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+    t.integer "row_order"
   end
 
 end
